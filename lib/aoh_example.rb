5b25a152773d63b3a323f4b6fa8dcd48e0d407b9
Joshua Singleton <jsingle345@gmail.com>
2020-05-02 00:51:38 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -6,8 +6,7 @@ PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
 KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
-def assembled_aoh
-  
+def assembled_aoh = 
    {:name => "Don Gately", :occupation => "Live-in Staff" }
    {:name => "Joelle van Dyne", :occupation =>"Radio Personality" }
    {:name => "Pat Monteseian", :occupation => "Staff" }
