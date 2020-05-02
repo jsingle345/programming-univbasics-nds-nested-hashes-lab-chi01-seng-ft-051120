7da1fe1bdccd3773534bada2d93ff0471729bb98
Joshua Singleton <jsingle345@gmail.com>
2020-05-02 00:52:47 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -11,7 +11,7 @@ def assembled_aoh
    {:name => "Joelle van Dyne", :occupation =>"Radio Personality" }
    {:name => "Pat Monteseian", :occupation => "Staff" }
    {:name => "Kate Gompert", :occupation => "None" }
-   {:name => "Bruce Green", :occupation => "Fan of Mildred" }
+   {:name => "Bruce Green", :occupation => "Fan of Mildred",}
    
 end
 
